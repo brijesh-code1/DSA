d05400ff43dc1300ab945f80138655e861036f98
brijesh-code1 <studybrijeshmnnit@gmail.com>
2025-09-30 19:47:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (10.93%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
-    int fib(int n) {
-        //BASE CASE
+
+    int fibon(int n, vector<int>&dp) 
+    {
+        //base case
         if(n<=1)
         return n;
+        //already calculated result
+        if(dp[n]!=-1)
+        return dp[n];
 
-        return fib(n-1)+fib(n-2);
+        return dp[n]= fibon(n-1,dp)+fibon(n-2,dp);
+    }
+    int fib(int n) {
+
+        vector<int>dp(n+1, -1);
+        return fibon(n,dp);
         
     }
 };
