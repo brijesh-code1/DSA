b1c19e57341fb3c4dae1e79b100ba03c5bf9aed3
brijesh-code1 <studybrijeshmnnit@gmail.com>
2025-09-30 20:11:39 +0530

    Time: 3 ms (66.41%) | Memory: 7.9 MB (36.20%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
 
-    int fibon(int n, vector<int>&dp) 
-    {
-        //base case
-        if(n<=1)
-        return n;
-        //already calculated result
-        if(dp[n]!=-1)
-        return dp[n];
-
-        return dp[n]= fibon(n-1,dp)+fibon(n-2,dp);
-    }
     int fib(int n) {
 
         if(n<=1)
         return n;
-
-        vector<int>dp(n+1, -1);
-        dp[0]=0;
-        dp[1]=1;
+        long long curr , prev=1, prev2=0;
 
         for(int i=2;i<=n;i++)
-        dp[i]=dp[i-1]+dp[i-2];
-        return dp[n];
+        {
+            curr = prev + prev2;
+            prev2=prev;
+            prev=curr;
+        }
+        return curr;
         
     }
 };
