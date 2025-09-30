a7aa479fa122bf18a21ce8dbfe861af1b25ca32e
brijesh-code1 <studybrijeshmnnit@gmail.com>
2025-09-30 20:01:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (10.93%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -14,8 +14,16 @@ public:
     }
     int fib(int n) {
 
+        if(n<=1)
+        return n;
+
         vector<int>dp(n+1, -1);
-        return fibon(n,dp);
+        dp[0]=0;
+        dp[1]=1;
+
+        for(int i=2;i<=n;i++)
+        dp[i]=dp[i-1]+dp[i-2];
+        return dp[n];
         
     }
 };
