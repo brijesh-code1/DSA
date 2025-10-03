b2a93bfc827ee8664ab7e33daac087cca7f20a4a
brijesh-code1 <studybrijeshmnnit@gmail.com>
2025-10-03 23:18:58 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (49.11%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,7 +1,26 @@
+
 class Solution {
 public:
+    // Function to sort the array containing only 0s, 1s, and 2s
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+
+        // Counting the number of 0s, 1s, and 2s in the array
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) cnt0++;
+            else if (nums[i] == 1) cnt1++;
+            else cnt2++;
+        }
+
+        /* Placing the elements in the
+        original array based on counts*/
+        //placing 0's
+        for (int i = 0; i < cnt0; i++) nums[i] = 0;
+
+        //placing 1's
+        for (int i = cnt0; i < cnt0 + cnt1; i++) nums[i] = 1; 
         
+        //placing 2's
+        for (int i = cnt0 + cnt1; i < nums.size(); i++) nums[i] = 2;
     }
 };
